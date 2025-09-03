50e585f6ea61b0568d64a51f54b475571dfc8cd3
Hà Minh Trí <trihaminh2004@gmail.com>
2025-09-04 00:25:48 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,6 +45,7 @@ testConnection().then(isConnected => {
   if (isConnected) {
     app.listen(PORT, () => {
       console.log(`Backend server is running on http://localhost:${PORT}`);
+      console.log(`Backend server is running on http://0.0.0.0:${PORT}`);
       console.log(`Environment: ${process.env.NODE_ENV}`);
     });
   } else {
